32e3f1341c201d4311522d495831524d00c493e9
RobertBZ <rbalderrama41@gmail.com>
2019-02-04 11:29:26 -0400

    Inicio tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'list', loadChildren: './list/list.module#ListPageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' }
