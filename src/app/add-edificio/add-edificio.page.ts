88504dba4e4450cc64c80b1bb39d39ed19c496e9
arodri <arodri.developer@gmail.com>
2019-02-05 11:23:17 -0400

    añadiendo modelo de edificios y estructura para agregar

--- src/app/add-edificio/add-edificio.page.ts
+++ src/app/add-edificio/add-edificio.page.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Edificio } from '../models/edificio';
-=======
 import { Router } from '@angular/router';
->>>>>>> 88bd5cbc2b05272f6b85f81f9344d91abec68518
 
 @Component({
   selector: 'app-add-edificio',
@@ -12,11 +9,10 @@ import { Router } from '@angular/router';
 })
 export class AddEdificioPage implements OnInit {
 
-<<<<<<< HEAD
   // variables de uso Externo e Interno
   edificio: Edificio;
 
-  constructor() { 
+  constructor(private router: Router) { 
     //Inicializacion
     this.edificio = new Edificio;
 
@@ -29,13 +25,7 @@ export class AddEdificioPage implements OnInit {
     console.log("Guardar Edificio -> ", this.edificio);
   }
 
-=======
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-  }
   return(){
     this.router.navigate(['home']);
   }
->>>>>>> 88bd5cbc2b05272f6b85f81f9344d91abec68518
 }
