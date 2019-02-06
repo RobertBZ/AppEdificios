6a1a1f976830e423130243f4522c6ffd6d439394
RobertBZ <rbalderrama41@gmail.com>
2019-02-06 11:03:20 -0400

    solucion de modal y router

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Events, LoadingController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import * as firebase from "firebase";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,8 +7,7 @@ import * as firebase from "firebase";
 })
 export class LoginPage implements OnInit {
 
-  constructor(public authen : AngularFireAuth, public afAuth: AngularFireAuth,private router: Router,public loadingController: LoadingController,
-    public events: Events) { }
+  constructor() { }
 
   ngOnInit() { }
   
