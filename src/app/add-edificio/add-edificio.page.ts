0baf046ffd21b3c74498ed3190fb144069522401
arodri <arodri.developer@gmail.com>
2019-02-06 12:26:06 -0400

    commit pendiente de agregar edificio

--- src/app/add-edificio/add-edificio.page.ts
+++ src/app/add-edificio/add-edificio.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵCompiler_compileModuleAndAllComponentsAsync__POST_R3__ } from '@angular/core';
 import { Edificio } from '../models/edificio';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { CanDeactivate } from '@angular/router/src/utils/preactivation';
+import { createNodeAtIndex } from '@angular/core/src/render3/instructions';
+import { Key } from 'protractor';
+
 
 @Component({
   selector: 'app-add-edificio',
@@ -13,32 +17,77 @@ export class AddEdificioPage implements OnInit {
   // variables de uso Externo e Interno
   edificio: Edificio;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     //Inicializacion
     this.edificio = new Edificio;
 
+
+    //Pruebas de consultas
+    this.listarEdificios();
   }
 
   ngOnInit() {
   }
 
-  async agregarEdificio(){
-    console.log("Guardar Edificio -> ", this.edificio);
-    let userOnly = 1; //usuario id
-    let cantidad = 2; //identificacion del edificio
-    firebase.database().ref('/edificio/'+userOnly+'/'+cantidad).set({
+  generadorID(){
+    let cadena: string;
+    cadena = this.edificio.nombre + "00" + this.edificio.expensas; 
+    return cadena;
+  }
+
+  async agregarEdificio() {
+    this.edificio._id = this.generadorID();
+
+    console.log("Guardar A Edificio -> ", this.edificio);
+    firebase.database().ref('/Edificio/' + this.edificio._id + '/').set({
       descripcion: this.edificio.descripcion,
       direccion: this.edificio.direccion,
       expensas: this.edificio.expensas,
       nombre: this.edificio.nombre,
       numeroDomicilio: this.edificio.nroDomicilio,
       numeroRecepcion: this.edificio.nroRecepcion,
-    })
+    });
+
+
+    /*  firebase.database().ref('/edificio/'+userOnly+'/').push({
+        descripcion: this.edificio.descripcion,
+        direccion: this.edificio.direccion,
+        expensas: this.edificio.expensas,
+        nombre: this.edificio.nombre,
+        numeroDomicilio: this.edificio.nroDomicilio,
+        numeroRecepcion: this.edificio.nroRecepcion,
+      })*/
+
     console.log("datos agregados");
-    this.return();
+    // this.return();
   }
 
-  return(){
+  async listarEdificios() {
+    let content: any;
+    let keys: any;
+    this.edificio._id = this.generadorID();
+    
+  await firebase.database().ref('/Edificio/').once('value').then((data) => {
+      keys = data.
+      content = data.val();
+    });
+
+    console.log("id: ", keys + " Contenido: ", content);
+  }
+
+  async buscarEdificio() {
+
+  }
+
+  async eliminarEdificio() {
+
+  }
+
+  async editarEdificio() {
+
+  }
+
+  return() {
     this.router.navigate(['home']);
   }
 }
--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -1,41 +1,100 @@
 import { Injectable } from '@angular/core';
 import * as firebase from "firebase";
+import { Edificio } from '../models/edificio';
+import { stringify } from 'querystring';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ServiciosService {
-  messages : any;
-  constructor() { }
+    i: number = 0;
+    resultado: any;
 
-  async obtenenerDatos(reference : any): Promise<any> {
-    return new Promise((resolve, reject) => {
-        firebase.database().ref(reference)
-            .once('value')
-            .then((querySnapshot) => {
-                let arr = [];
-                querySnapshot.forEach(function (doc) {
-                    var obj = JSON.parse(JSON.stringify(doc.val()));
-                    arr.push(obj);
+    constructor() { }
+
+    async obtenenerDatos(reference: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            firebase.database().ref(reference)
+                .once('value')
+                .then((querySnapshot) => {
+                    let arr = [];
+                    //console.log(querySnapshot.val());
+                    querySnapshot.forEach(function (doc) {
+                        // console.log(doc.val());
+                        var obj = JSON.parse(JSON.stringify(doc.val()));
+                        arr.push(obj);
+                    });
+                    if (arr.length > 0) {
+                        resolve(arr);
+
+                    } else {
+                        resolve(null);
+                    }
+                })
+                .catch((error: any) => {
+                    reject(error);
                 });
-                if (arr.length > 0) {
-                    resolve(arr);
-                    
-                } else {
-                    resolve(null);
-                }
-            })
-            .catch((error: any) => {
-                reject(error);
-            });
         });
-  }
+    }
 
-  async nombreEdificios() {
-    var o = 0;
-    await this.obtenenerDatos("edificiosRegistrados").then((e)=>{
-    console.log(e);
-        this.messages = e;
-    });
-  }
+    async nombreEdificios() {
+        var o = 0;
+        await this.obtenenerDatos("edificiosRegistrados").then((e) => {
+            console.log(e);
+            this.resultado = e;
+        });
+    }
+
+    async getEdidicios() {
+        /* ***var res = [];
+         var catidadedificio = null;
+         await this.obtenenerDatos("edificio/1").then((data) => {
+             catidadedificio = data;
+         });*/
+        /*
+        await firebase.database().ref('/edificio/1/').once('value').then((data) => {
+             this.resultado = data.val();
+             console.log("DOS:  ", this.resultado);
+         });*/
+        /* var aux = this.resultado.filter(datos => datos.nombre[this.i])[this.i].nombre
+         console.log("Comment    ", aux);*/
+        //**** */console.log("cantidad: ", catidadedificio.length);
+
+        /**** for (let i = 1; i <= catidadedificio.length; i++) {
+             await firebase.database().ref('/edificio/1/' + i).once('value').then((data) => {
+                 res.push(data.val());
+                 console.log(data.val());
+             });
+         } */
+
+        /*  await firebase.database().ref('/edificio/1/1').once('value').then((data) => {
+              res = data.val();
+          });*/
+
+        // console.log("DOS:  ", res);
+
+
+        // POR MEDIO DE ID
+        let nombres: String;
+        let id: String;
+        let listKeys;
+
+        await firebase.database().ref('/edificio/1/').once('value').then((data) => {
+            listKeys = data.toJSON();
+        });
+
+        console.log("listKey: ", listKeys);
+
+  /*      var datos = { _id: id, contenido: nombres }
+        let res = [];
+        await firebase.database().ref('/edificio/1/').once('value').then((data) => {
+            //res.push(data.val());
+            datos._id = data.key;
+            datos.contenido = data.val();
+
+            console.log("Objeto:  ", datos._id, datos.contenido);
+        });
+*/
+        return listKeys;
+    }
 }
